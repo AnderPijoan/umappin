63dfd950355d2f292d0bd12849d6f834f88feaf6
Jon Arriaga <jon.arriaga@opendeusto.es>
2013-03-12 09:24:29 +0100

    Error validating DiscussionHeader corrected. Ready for Oscar to pull

--- public/js/messagesAPP/model/discussionHeader.js
+++ public/js/messagesAPP/model/discussionHeader.js
@@ -34,9 +34,9 @@ var messagesApp = messagesApp || {};
 				console.error(attrs);
 			}
 		},validate: function(attrs){
-		     if !(attrs != null &&attrs.id != null && attrs.subject !=null &&
-             				attrs.message_number != null && attrs.unread_messages !=null &&
-             				attrs.user !=null){
+		     if (attrs == null &&attrs.id == null || attrs.subject ==null ||
+             				attrs.message_number == null || attrs.unread_messages ==null ||
+             				attrs.user ==null){
              				return "Error creating a DiscussionHeader";
              }
 		}
