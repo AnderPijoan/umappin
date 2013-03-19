67b20f41be4084024beb2f5c343bf6bbc1bf7ec9
orodriguez <orodriguez@opendeusto.es>
2013-03-19 22:11:20 +0100

    minor change

--- public/js/messagesAPP/view/receivedMessages_view.js
+++ public/js/messagesAPP/view/receivedMessages_view.js
@@ -12,7 +12,7 @@ var messagesApp = messagesApp || {};
 		render: function(){
 			//return the template with the info of the model and changed to 
 			//messages_body to have this one as the messages APP body
-			$('#messages_body').html(this.template(this.model.toJSON()));
+			$(this.el).html(this.template(this.model.toJSON()));
 			console.log("render received Discussions");
 			return this;
 		}
