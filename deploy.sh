7f4aed324619ebf41b73f3b82f1fc712aa0f0394
ugarcia <unai.garcia@opendeusto.es>
2013-04-01 23:31:49 +0200

    trying to automate this...

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,9 @@ export http_proxy=http://proxy-s-priv.deusto.es:3128/
 cd /usr/share/umappin
 unset $(git rev-parse --local-env-vars)
 git pull origin master
-#pidof java |xargs kill 
+pidof java |xargs kill 
 play clean compile stage
 for x in 000 100;
  do 
-   kill -KILL $(cat p$x) 
    target/start -Dpidfile.path=p$x -Dhttp.port=9$x&	
 done
