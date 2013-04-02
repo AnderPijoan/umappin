399c5c54aa6ac9fd08330daea1d1a7b253d21bf8
ugarcia <unai.garcia@opendeusto.es>
2013-04-02 17:47:26 +0200

    autodeploy a standalone play instance too

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ unset $(git rev-parse --local-env-vars)
 git pull origin master
 pidof java |xargs kill 
 play clean compile stage
-rm -r /var/www/umappin/.
+rm -r /var/www/umappin
+mkdir var/www/umappin
 cp -r target/scala-2.10/resource_managed/main/public/. /var/www/umappin/
 cp -r public/. /var/www/umappin/
 for x in 000 100; do 
