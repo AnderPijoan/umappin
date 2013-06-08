a5d1121f5ecf27bb8ce60f0116362ab5df91922c
Antonio Digangi <antonio.digangi@opendeusto.es>
2013-06-08 16:54:03 +0200

    partial commit on photo thumbnails

---
+++ app/utils/ImageResizerActor.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: a.digangi
+ * Date: 03/06/13
+ * Time: 19.17
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImageResizerActor {
+}
