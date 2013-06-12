1ebd6c3ff15176882c611763e64f6db6311bcc7b
Iosu Garcia <igarrieta1235@gmail.com>
2013-06-12 22:34:06 +0200

    Activity implemented.

--- public/js/timelineApp/routers/router.js
+++ public/js/timelineApp/routers/router.js
@@ -4,7 +4,7 @@ var timelineApp = timelineApp || {};
 timelineApp.Router = Backbone.SubRoute.extend({
 
 	routes: {
-		'comment/:id' : 	'comments',
+		'comment/:id' : 		'comments',
 		'news' :			    'news',
 		'/'	: 				    'publicationHeaders',
 		'user/:id' :		  	'userWall'
@@ -12,115 +12,128 @@ timelineApp.Router = Backbone.SubRoute.extend({
 
 	comments: function(idComment) {
 		this.loadTemplateIfNeed(function() {
-      requirejs(['/assets/js/timelineApp/model/publication.js'], function() {
-        requirejs(['/assets/js/timelineApp/model/comment.js'], function() {
-          requirejs(['/assets/js/timelineApp/view/publicationComments_view.js'], function() {
-            publication = new timelineApp.Publication({id:idComment});
-            console.log(idComment);
-            publication.fetch({
-                success: function() {
-                  $('#new_post').hide();
-                  view = new timelineApp.CommentsView({ model: publication });
-                  $('#comments_body').html(view.addTimeAgoAndRender().el);
-                },
-                error: function() {
-                  console.log("Error getting comments from server");
-                }
-            });
-          });
-        });
-      });
+			requirejs(['/assets/js/timelineApp/model/publication.js'], function() {
+				requirejs(['/assets/js/timelineApp/model/comment.js'], function() {
+					requirejs(['/assets/js/timelineApp/view/publicationComments_view.js'], function() {
+						publication = new timelineApp.Publication({id:idComment});
+						console.log(idComment);
+						publication.fetch({
+							success: function() {
+								$('#new_post').hide();
+								view = new timelineApp.CommentsView({ model: publication });
+								$('#comments_body').html(view.addTimeAgoAndRender().el);
+							},
+							error: function() {
+								console.log("Error getting comments from server");
+							}
+						});
+					});
+				});
+			});
 		});
 	},
 
 	news: function() {
-		this.loadNewsTemplateIfNeed(function() {
-      requirejs(['/assets/js/timelineApp/model/publication.js'], function() {
-        requirejs(['/assets/js/timelineApp/model/comment.js'], function() {
-          requirejs(['/assets/js/timelineApp/view/receivedComments_view.js'], function() {
-            requirejs(['/assets/js/timelineApp/collection/newsCollection.js'], function() {
-              timelineApp.NewsCollection.fetch();
-            });
-          });
-        });
-      });
+		//this.loadTemplateIfNeed(function() {
+		that = this;
+		setTemplate ("/assets/templates/activity.html", function() {
+			requirejs(['/assets/js/timelineApp/model/publication.js'], function() {
+				requirejs(['/assets/js/timelineApp/model/comment.js'], function() {
+					requirejs(['/assets/js/timelineApp/collection/publicationCollection.js'], function() {
+						requirejs(['/assets/js/timelineApp/collection/newsCollection.js'], function() {
+							requirejs(['/assets/js/timelineApp/view/receivedNews_view.js'], function() {
+								requirejs(['/assets/js/timelineApp/view/commentsApp_view.js'], function() {
+									requirejs(['/assets/js/timelineApp/view/newsApp_view.js'], function() {
+										console.log("routing received");
+										// Create our global collection of **Publications**.
+										that.cleanUpViews();
+										timelineApp.newsAppView =  new timelineApp.NewsAppView();
+										timelineApp.NewsCollection.reset();
+										timelineApp.NewsCollection.fetch();
+									});
+								});
+							});
+						});
+					});
+				});
+			});
 		});
 	},
 
 	publicationHeaders: function() {
 		//this.loadTemplateIfNeed(function() {
-    that = this;
-    setTemplate ("/assets/templates/wall.html", function() {
-      requirejs(['/assets/js/timelineApp/model/publication.js'], function() {
-        requirejs(['/assets/js/timelineApp/model/comment.js'], function() {
-          requirejs(['/assets/js/timelineApp/collection/publicationCollection.js'], function() {
-            requirejs(['/assets/js/timelineApp/collection/newsCollection.js'], function() {
-              requirejs(['/assets/js/timelineApp/view/receivedComments_view.js'], function() {
-                requirejs(['/assets/js/timelineApp/view/commentsApp_view.js'], function() {
-                  requirejs(['/assets/js/timelineApp/view/publicationComments_view.js'], function() {
-                    console.log("routing received");
-                    // Create our global collection of **Publications**.
-                    that.cleanUpViews();
-                    $('#new_post').show();
-                    $('#user_wall').hide();
-                    timelineApp.appView =  new timelineApp.AppView();
-                    timelineApp.PublicationCollection.reset();
-                    timelineApp.PublicationCollection.fetch();
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
+		that = this;
+		setTemplate ("/assets/templates/wall.html", function() {
+			requirejs(['/assets/js/timelineApp/model/publication.js'], function() {
+				requirejs(['/assets/js/timelineApp/model/comment.js'], function() {
+					requirejs(['/assets/js/timelineApp/collection/publicationCollection.js'], function() {
+						requirejs(['/assets/js/timelineApp/collection/newsCollection.js'], function() {
+							requirejs(['/assets/js/timelineApp/view/receivedComments_view.js'], function() {
+								requirejs(['/assets/js/timelineApp/view/commentsApp_view.js'], function() {
+									requirejs(['/assets/js/timelineApp/view/publicationComments_view.js'], function() {
+										console.log("routing received");
+										// Create our global collection of **Publications**.
+										that.cleanUpViews();
+										$('#new_post').show();
+										$('#user_wall').hide();
+										timelineApp.appView =  new timelineApp.AppView();
+										timelineApp.PublicationCollection.reset();
+										timelineApp.PublicationCollection.fetch();
+									});
+								});
+							});
+						});
+					});
+				});
+			});
 		});
 	},
 
 	userWall: function(idUser){
-    //this.loadTemplateIfNeed(function() {
-    that = this;
-    setTemplate ("/assets/templates/wall.html", function() {
-      requirejs(['/assets/js/timelineApp/model/publication.js'], function() {
-        requirejs(['/assets/js/timelineApp/model/userPublication.js'], function() {
-          requirejs(['/assets/js/timelineApp/model/comment.js'], function() {
-            requirejs(['/assets/js/timelineApp/collection/userPublicationCollection.js'], function() {
-              requirejs(['/assets/js/timelineApp/collection/newsCollection.js'], function() {
-                requirejs(['/assets/js/timelineApp/view/receivedComments_view.js'], function() {
-                  requirejs(['/assets/js/timelineApp/view/userCommentsApp_view.js'], function() {
-                    requirejs(['/assets/js/timelineApp/view/publicationComments_view.js'], function() {
-                        requirejs(['/assets/js/account/models/user_model.js'], function() {
-                            requirejs(['/assets/js/account/models/follow_model.js'], function() {
-                                requirejs(['/assets/js/account/models/usrfollows_model.js'], function() {
-                                    requirejs(['/assets/js/account/models/usrfollowed_model.js'], function() {
-                                        requirejs(['/assets/js/account/views/user_follows_view.js'], function() {
-                      var searchedUser = new Account.User({id: idUser})
-                      searchedUser.fetch({
-                          complete: function() {
-                              var img = searchedUser.get('profilePicture');
-                              var profileImg = (img != null) ? './photos/'+ img +'/content' : './assets/img/140x140.gif';
-                              $('#user-wall-picture').html('<img id="user-wall-avatar" src="'+profileImg+'" onload="resize(this,128)">');
-                              $('#user-wall-nick h3').text(searchedUser.get('name'));
-                          }
-                      });
-                      var follows = new Account.UsrFollows({id: Account.session.get('id')});
-                      follows.fetch({
-                          complete: function(data) {
-                              var followed = new Account.UsrFollowed({id: idUser});
-                              followed.fetch({
-                                  complete: function(data) {
-                                      var followsView = new Account.UserFollowsView({
-                                          model: searchedUser,
-                                          follows: follows,
-                                          followed: followed
-                                      });
-                                      $('#user-wall-follow').html(followsView.render().el);
-                                  }
-                              });
-                          }
-                      });
+		//this.loadTemplateIfNeed(function() {
+		that = this;
+		setTemplate ("/assets/templates/wall.html", function() {
+			requirejs(['/assets/js/timelineApp/model/publication.js'], function() {
+				requirejs(['/assets/js/timelineApp/model/userPublication.js'], function() {
+					requirejs(['/assets/js/timelineApp/model/comment.js'], function() {
+						requirejs(['/assets/js/timelineApp/collection/userPublicationCollection.js'], function() {
+							requirejs(['/assets/js/timelineApp/collection/newsCollection.js'], function() {
+								requirejs(['/assets/js/timelineApp/view/receivedComments_view.js'], function() {
+									requirejs(['/assets/js/timelineApp/view/userCommentsApp_view.js'], function() {
+										requirejs(['/assets/js/timelineApp/view/publicationComments_view.js'], function() {
+											requirejs(['/assets/js/account/models/user_model.js'], function() {
+												requirejs(['/assets/js/account/models/follow_model.js'], function() {
+													requirejs(['/assets/js/account/models/usrfollows_model.js'], function() {
+														requirejs(['/assets/js/account/models/usrfollowed_model.js'], function() {
+															requirejs(['/assets/js/account/views/user_follows_view.js'], function() {
+																var searchedUser = new Account.User({id: idUser})
+																searchedUser.fetch({
+																	complete: function() {
+																		var img = searchedUser.get('profilePicture');
+																		var profileImg = (img != null) ? './photos/'+ img +'/content' : './assets/img/140x140.gif';
+																		$('#user-wall-picture').html('<img id="user-wall-avatar" src="'+profileImg+'" onload="resize(this,128)">');
+																		$('#user-wall-nick h3').text(searchedUser.get('name'));
+																	}
+																});
+																var follows = new Account.UsrFollows({id: Account.session.get('id')});
+																follows.fetch({
+																	complete: function(data) {
+																		var followed = new Account.UsrFollowed({id: idUser});
+																		followed.fetch({
+																			complete: function(data) {
+																				var followsView = new Account.UserFollowsView({
+																					model: searchedUser,
+																					follows: follows,
+																					followed: followed
+																				});
+																				$('#user-wall-follow').html(followsView.render().el);
+																			}
+																		});
+																	}
+																});
 
 
-                      /*
+																/*
                       var userFollowProfile = $.get('/userfollowed/'+idUser),
                           follow = idUser;
                       userFollowProfile.done(function( data ) {
@@ -147,26 +160,26 @@ timelineApp.Router = Backbone.SubRoute.extend({
                             sessionStorage.setItem('wall-follow-user',follow);
                       });
                       console.log("User " + idUser + " Wall");
-                      */
-                      that.cleanUpViews();
-                      timelineApp.userAppView = new timelineApp.UserAppView();
-                      timelineApp.UserPublicationCollection.url = "/userpublications/" + idUser;
-                      timelineApp.UserPublicationCollection.reset();
-                      timelineApp.UserPublicationCollection.fetch();
-                                });
-                              });
-                            });
-                          });
-                        });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    });
+																 */
+																that.cleanUpViews();
+																timelineApp.userAppView = new timelineApp.UserAppView();
+																timelineApp.UserPublicationCollection.url = "/userpublications/" + idUser;
+																timelineApp.UserPublicationCollection.reset();
+																timelineApp.UserPublicationCollection.fetch();
+															});
+														});
+													});
+												});
+											});
+										});
+									});
+								});
+							});
+						});
+					});
+				});
+			});
+		});
 	},
 
 	loadTemplateIfNeed:function(callback){
@@ -199,17 +212,17 @@ timelineApp.Router = Backbone.SubRoute.extend({
 		}
 	},
 
-  cleanUpViews: function() {
-    if (timelineApp.appView && timelineApp.appView != undefined) {
-      console.log(timelineApp.appView);
-      timelineApp.appView.unbind();
-      timelineApp.appView.remove();
-    }
-    if (timelineApp.userAppView && timelineApp.userAppView != undefined) {
-      console.log(timelineApp.userAppView);
-      timelineApp.userAppView.unbind();
-      timelineApp.userAppView.remove();
-    }
-  }
+	cleanUpViews: function() {
+		if (timelineApp.appView && timelineApp.appView != undefined) {
+			console.log(timelineApp.appView);
+			timelineApp.appView.unbind();
+			timelineApp.appView.remove();
+		}
+		if (timelineApp.userAppView && timelineApp.userAppView != undefined) {
+			console.log(timelineApp.userAppView);
+			timelineApp.userAppView.unbind();
+			timelineApp.userAppView.remove();
+		}
+	}
 
 });
--- public/js/timelineApp/view/newsApp_view.js
+++ public/js/timelineApp/view/newsApp_view.js
@@ -3,12 +3,12 @@ var timelineApp = timelineApp || {};
 //this is the view controller for the all comments APP
 (function(){
 	timelineApp.NewsAppView = Backbone.View.extend({
-		el: 'body',
+		el: 'div#wallDiv',
 
 		initialize: function () {
 			//triggered on sync
 			this.listenTo(timelineApp.NewsCollection, 'sync', this.renderNews);
-
+			this.renderNews();
 		},
 		//Publication Headers methods
 		
@@ -17,15 +17,9 @@ var timelineApp = timelineApp || {};
 			$('#news_body').html('<ul id="publication_list"> </ul>');
 			timelineApp.NewsCollection.sort();
 			timelineApp.NewsCollection.each(function(publication){
-				var date = new Date(publication.get("timeStamp"));
-				publication.set({"date": date});
-			   	var view = new timelineApp.ReceivedView({ model: publication});
+			   	var view = new timelineApp.ReceivedNewsView({ model: publication});
 			   	$('#publication_list').prepend(view.render().el);
 			});
 		}		
 	});
-	
-	newsAppView = (typeof newsAppView === 'undefined') ? new timelineApp.NewsAppView() : newsAppView;
-	
 })();
-	
