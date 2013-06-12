54bbe0618350fb4b97b563de21130d9a83769916
Ricardo <ricardo.berrocal@opendeusto.es>
2013-06-12 16:31:02 +0200

    statistics

--- public/js/statisticsApp/view/statisticView.js
+++ public/js/statisticsApp/view/statisticView.js
@@ -4,11 +4,19 @@ _.templateSettings.variable = "rc";
 //this is the view controller for the Statistics
 (function(){
 	statisticsApp.StatisticView = Backbone.View.extend({
-		
+		initialize: function() {
+			console.log(this.model.get('statistics'));
+            var currentUser = JSON.parse(sessionStorage.getItem('user')),
+				statistics = this.model.get('statistics');
+			statistics.INVITEFRIENDS = localStorage.getItem('invite-friend-award'+currentUser.id);
+			if(!statistics.INVITEFRIENDS){
+				statistics.INVITEFRIENDS = 0;
+			}
+		},
 		setTemplate: function() {
 			this.template = _.template($('#statistics-template').html());
 		},
-		
+
 		render:function () {
 			$(this.el).html(this.template(this.model.toJSON()));
             return this;
